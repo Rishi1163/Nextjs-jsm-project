7a270f3451007fd14b5321f7c7ee336239d470ce
Rishi Jadhav <rishijadhav935@gmail.com>
2026-02-09 18:22:56 +0530

    small fix

--- components/BookEvent.tsx
+++ components/BookEvent.tsx
@@ -10,13 +10,13 @@ const BookEvent = ({eventId, slug}: {eventId: string, slug: string}) => {
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault()
-        const {success, error} = await createBooking({eventId, slug, email})
+        const {success} = await createBooking({eventId, slug, email})
         if(success) {
             setSubmitted(true)
             posthog.capture('event_booked', {eventId, slug, email})
         } else {
-            console.error("Booking creation failes", error)
-            posthog.captureException(error)
+            console.error("Booking creation failes")
+            posthog.captureException("Booking creation failes")
         }
         setTimeout(()=>{
             setSubmitted(true)
